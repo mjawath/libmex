cd40b27ab059d908fbf20df8863792c6cd4e85ef
mjawath <mjawath@2fe23c1b-34cb-1b19-2e7e-fe0f4ac1c3c0>
2011-12-07 00:33:43 +0000

    u8pdated the table editing

--- trunk/InvoicingSystemTU/src/invoicingsystem/InvoiceTestUI2.java
+++ trunk/InvoicingSystemTU/src/invoicingsystem/InvoiceTestUI2.java
@@ -11,31 +11,23 @@
 package invoicingsystem;
 
 import com.sun.awt.AWTUtilities;
-import java.awt.Color;
-import java.awt.Component;
-import java.awt.Dimension;
-import java.awt.Event;
+import java.awt.Dialog.ModalityType;
 import java.awt.event.ActionEvent;
 import java.awt.event.FocusAdapter;
 import java.awt.event.FocusEvent;
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
-import java.awt.event.MouseEvent;
-import java.awt.event.MouseMotionAdapter;
+import java.awt.event.WindowEvent;
+import java.awt.event.WindowFocusListener;
 import javax.swing.AbstractAction;
-import javax.swing.AbstractCellEditor;
-import javax.swing.BorderFactory;
-import javax.swing.JComponent;
 import javax.swing.JDialog;
-import javax.swing.JLayeredPane;
 import javax.swing.JPanel;
 import javax.swing.JTable;
-import javax.swing.JTextField;
+import javax.swing.JTextArea;
 import javax.swing.KeyStroke;
+import javax.swing.SwingUtilities;
 import javax.swing.event.ListSelectionEvent;
 import javax.swing.event.ListSelectionListener;
-import javax.swing.table.DefaultTableCellRenderer;
-import javax.swing.table.TableCellEditor;
 import org.components.windows.TabPanelUI;
 
 /**
@@ -47,33 +39,32 @@ public class InvoiceTestUI2 extends TabPanelUI {
     /** Creates new form InvoiceTestUI */
     public InvoiceTestUI2() {
         initComponents();
+
         cxTable1.putClientProperty("JTable.autoStartsEdit", Boolean.TRUE);
-            
-           jd= new jd();
-           jd.getRootPane().setOpaque(false);
-           AWTUtilities.setWindowOpacity(jd, 0.8f);
-           jd.setUndecorated(true);
-                
-                jd.setSize(700,100);
-        JLayeredPane layeredPane = new JLayeredPane();
-        layeredPane.setPreferredSize(new Dimension(10, 310));
-        layeredPane.setBackground(Color.red);
-        layeredPane.setBorder(BorderFactory.createTitledBorder(
-                "Move the Mouse to Move Duke"));
-        layeredPane.addMouseMotionListener(new MouseMotionAdapter() {
+
+        lineItemDialogPanel = new DetailPanel();
+        lineItemDialogPanel.setTable(cxTable1);
+
+        lineItemDialogPanel.addFocusListener(new FocusAdapter() {
 
             @Override
-            public void mouseMoved(MouseEvent e) {
-                System.out.println("e " + e);
+            public void focusGained(FocusEvent e) {
+        
+                lineItemDialogPanel.setFocus();
             }
+        
         });
-        add(layeredPane);
         
         cxTable1.addKeyListener(new KeyAdapter() {
 
             @Override
             public void keyPressed(KeyEvent e) {
-                System.out.println("pressed " + e);
+//                jd.setFocus();
+                if (e.getKeyCode() == KeyEvent.VK_DOWN || e.getKeyCode() == KeyEvent.VK_UP) {
+                    return;
+                }
+                lineItemDialogPanel.dispatchEventx(e);
+
             }
 
             @Override
@@ -86,8 +77,9 @@ public class InvoiceTestUI2 extends TabPanelUI {
 //                System.out.println("released " + e);
             }
         });
+        jDialog.add(new JTextArea("ddddddddddd"));
         cxTable1.setSurrendersFocusOnKeystroke(true);
-//        cxTable1.getColumn(0).setCellEditor(new MyTableCellEditor());
+        cxTable1.getColumn(0).setCellEditor(new IntegerEditor(100, 300));
 //        cxTable1.getColumn(1).setCellEditor(new MyTableCellEditor());
 //        cxTable1.getColumn(2).setCellEditor(new MyTableCellEditor());
 //        cxTable1.getColumn(3).setCellEditor(new MyTableC/ellEditor());
@@ -101,44 +93,61 @@ public class InvoiceTestUI2 extends TabPanelUI {
                         System.out.println(" time " + System.currentTimeMillis());
                     }
                 });
-
         cxTable1.setDefaultRenderer(String.class, new CustomRenderer());
+
         cxTable1.getSelectionModel().addListSelectionListener(new ListSelectionListener() {
 
             public void valueChanged(ListSelectionEvent e) {
-            }
-        });
-        cxTable1.getSelectionModel().addListSelectionListener(new ListSelectionListener() {
-            public void valueChanged(ListSelectionEvent e) {
-   
+
                 int x = (int) cxTable1.getLocationOnScreen().x;
                 int y = (int) cxTable1.getLocationOnScreen().y;
                 int sr = cxTable1.getSelectedRow();
-                int hight= (sr+1)*cxTable1.getRowHeight();
-                jd.setLocation(x,y+hight);
-                jd.setFocusable(false);
-                jd.setVisible(true);
-                
-            }
-        });
-        cxTable1.addFocusListener(new FocusAdapter() {
+                int hight = (sr + 1) * cxTable1.getRowHeight();
+                lineItemDialogPanel.setLocation(x, y + hight);
+//                lineItemDialogPanel.setFocusable(false);
+                lineItemDialogPanel.showWithoutFocus();
 
-            @Override
-            public void focusLost(FocusEvent e) {
-        jd.setVisible(false);
+
+
             }
-        
         });
+        lineItemDialogPanel.addWindowFocusListener(new WindowFocusListener() {
+
+            public void windowGainedFocus(WindowEvent e) {
+                //do nothing
+            }
+
+            public void windowLostFocus(WindowEvent e) {
+
+                lineItemDialogPanel.setVisible(false);
+            }
+        });
+
     }
 
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        cPanel1 = new org.components.containers.CPanel();
+        cButton1 = new org.components.controls.CButton();
+        cTextField2 = new org.components.controls.CTextField();
         jScrollPane1 = new javax.swing.JScrollPane();
         cxTable1 = new org.components.controls.CxTable();
         cTextField1 = new org.components.controls.CTextField();
+        cButton2 = new org.components.controls.CButton();
 
         setLayout(null);
+
+        cPanel1.setLayout(null);
+        cPanel1.add(cButton1);
+        cButton1.setBounds(80, 50, 65, 23);
+
+        cTextField2.setText("cTextField2");
+        cPanel1.add(cTextField2);
+        cTextField2.setBounds(20, 10, 110, 25);
+
+        add(cPanel1);
+        cPanel1.setBounds(20, 350, 640, 80);
 
         cxTable1.setModel(new javax.swing.table.DefaultTableModel(
             new Object [][] {
@@ -159,7 +168,7 @@ public class InvoiceTestUI2 extends TabPanelUI {
                 java.lang.String.class, java.lang.String.class, java.lang.String.class, java.lang.String.class, java.lang.String.class, java.lang.String.class
             };
             boolean[] canEdit = new boolean [] {
-                false, false, true, false, false, true
+                true, false, false, false, false, false
             };
 
             public Class getColumnClass(int columnIndex) {
@@ -173,24 +182,59 @@ public class InvoiceTestUI2 extends TabPanelUI {
         jScrollPane1.setViewportView(cxTable1);
 
         add(jScrollPane1);
-        jScrollPane1.setBounds(10, 110, 660, 400);
+        jScrollPane1.setBounds(10, 110, 660, 200);
 
         cTextField1.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 cTextField1ActionPerformed(evt);
             }
         });
+        cTextField1.addKeyListener(new java.awt.event.KeyAdapter() {
+            public void keyReleased(java.awt.event.KeyEvent evt) {
+                cTextField1KeyReleased(evt);
+            }
+        });
         add(cTextField1);
         cTextField1.setBounds(40, 40, 130, 25);
-    }// </editor-fold>//GEN-END:initComponents
- JDialog jd = null;
-    private void cTextField1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_cTextField1ActionPerformed
 
-        cxTable1.requestFocus();
-        cxTable1.editCellAt(0, 0);
+        cButton2.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                cButton2ActionPerformed(evt);
+            }
+        });
+        add(cButton2);
+        cButton2.setBounds(150, 460, 65, 23);
+    }// </editor-fold>//GEN-END:initComponents
+    DetailPanel lineItemDialogPanel = null;
+    JDialog jDialog = new JDialog();
+
+    private void cTextField1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_cTextField1ActionPerformed
     }//GEN-LAST:event_cTextField1ActionPerformed
+    int y = 100;
+    private void cButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_cButton2ActionPerformed
+
+        y += 10;
+        cPanel1.setBounds(50, y, 600, 300);
+        this.revalidate();
+
+
+    }//GEN-LAST:event_cButton2ActionPerformed
+
+    private void cTextField1KeyReleased(java.awt.event.KeyEvent evt) {//GEN-FIRST:event_cTextField1KeyReleased
+        jDialog.setModal(false);
+        jDialog.setModalityType(ModalityType.MODELESS);
+        jDialog.setSize(200, 300);
+        jDialog.setFocusableWindowState(false);
+        jDialog.setVisible(true);
+        jDialog.setFocusableWindowState(false);
+        // TODO add your handling code here:
+    }//GEN-LAST:event_cTextField1KeyReleased
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private org.components.controls.CButton cButton1;
+    private org.components.controls.CButton cButton2;
+    private org.components.containers.CPanel cPanel1;
     private org.components.controls.CTextField cTextField1;
+    private org.components.controls.CTextField cTextField2;
     private org.components.controls.CxTable cxTable1;
     private javax.swing.JScrollPane jScrollPane1;
     // End of variables declaration//GEN-END:variables
@@ -205,4 +249,3 @@ public class InvoiceTestUI2 extends TabPanelUI {
         return this;
     }
 }
-
